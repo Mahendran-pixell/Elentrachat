0559477c8f0f8454f9c068f71439b44dc56476af
Mahendran-pixell <mahendran600@gmail.com>
2026-03-03 21:14:43 +0530

    Level system added

--- bot.py
+++ bot.py
@@ -1,14 +1,28 @@
-import os
+import asyncios
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 TOKEN = os.getenv("TOKEN")
 
 waiting_users = []
 active_chats = {}
+user_stats = {}
+
+def get_level(count):
+    if count >= 31:
+        return "👑 Legend"
+    elif count >= 16:
+        return "🔥 Chat Pro"
+    elif count >= 6:
+        return "💬 Social Starter"
+    else:
+        return "🌱 Explorer"
 
 # START COMMAND
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.chat_id
+
+    await update.message.reply_text("🔎 Searching for someone interesting...")
+    await asyncio.sleep(1.5)
 
     if user_id in active_chats:
         await update.message.reply_text("⚠️ You are already connected. Use /next to skip.")
@@ -24,7 +38,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         active_chats[user_id] = partner
         active_chats[partner] = user_id
 
-        await update.message.reply_text("✅ Connected to a stranger! Say hi 👋")
+         level = get_level(user_stats.get(user_id, 0))
+         await update.message.reply_text(f"✅ Connected!\n🏆 Your Level: {level}\nSay hi 👋")
         await context.bot.send_message(partner, "✅ Connected to a stranger! Say hi 👋")
     else:
         waiting_users.append(user_id)
@@ -37,6 +52,9 @@ async def next_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if user_id in active_chats:
         partner = active_chats.pop(user_id)
         active_chats.pop(partner, None)
+
+        user_stats[user_id] = user_stats.get(user_id, 0) + 1
+        user_stats[partner] = user_stats.get(partner, 0) + 1
 
         await context.bot.send_message(partner, "⚠️ Stranger disconnected.")
 
